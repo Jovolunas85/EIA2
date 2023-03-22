daaf050f0d830098c61304a02054766beca5a2bb
Medin <115719571+Jovolunas85@users.noreply.github.com>
2023-03-22 18:51:26 +0100

    dcdv

--- A01_poem/Script.js
+++ A01_poem/Script.js
@@ -2,18 +2,24 @@
 var Aufgabe1;
 (function (Aufgabe1) {
     let subjekte = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-    let praedikate = ["braut", "liebt", "studiert", "hasst", "zaubert"];
+    let praedikate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstroert"];
     let objekte = ["Zaubertraenke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-    console.log(subjekte, praedikate, objekte);
+    //console.log(subjekte, praedikate, objekte);
     let zk;
-    getVerse("hallo", "moin", "servus");
+    getVerse(subjekte[Math.floor(Math.random() * 7)], praedikate[Math.floor(Math.random() * 7)], objekte[Math.floor(Math.random() * 7)]);
     for (let i = subjekte.length; i >= 1; i--) {
-        console.log(i);
-        console.log(zk);
+        //console.log(i);
+        //console.log(zk);
     }
     function getVerse(_sub, _pra, _obj) {
-        zk = "Alohomora";
-        return zk;
+        let verse = "";
+        let zufall = Math.floor(Math.random() * subjekte.length);
+        console.log(zufall);
+        verse = "" + subjekte[zufall];
+        console.log(verse + " ");
+        subjekte.splice(zufall, 1);
+        //zk = _sub+ " " + _pra + " " +_obj;
+        //return zk;
     }
 })(Aufgabe1 || (Aufgabe1 = {}));
 //# sourceMappingURL=Script.js.map
--- A01_poem/Script.ts
+++ A01_poem/Script.ts
@@ -1,16 +1,22 @@
 namespace Aufgabe1 {
     let subjekte: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-    let praedikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert"];
+    let praedikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstroert"];
     let objekte: string[] = ["Zaubertraenke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-    console.log(subjekte, praedikate, objekte);
+    //console.log(subjekte, praedikate, objekte);
     let zk: string;
-    getVerse("hallo","moin","servus");
+    getVerse(subjekte[Math.floor(Math.random() * 7)], praedikate[Math.floor(Math.random() * 7)], objekte[Math.floor(Math.random() * 7)]);
     for (let i: number = subjekte.length; i >= 1; i--) {
-        console.log(i);
-        console.log(zk);
+        //console.log(i);
+        //console.log(zk);
     }
     function getVerse(_sub: string, _pra: string, _obj: string) {
-        zk = "Alohomora";
-        return zk;
+        let verse: string = "";
+        let zufall: number = Math.floor(Math.random() * subjekte.length);
+        console.log(zufall);
+        verse = "" + subjekte[zufall];
+        console.log(verse + " ");
+        subjekte.splice(zufall, 1);
+        //zk = _sub+ " " + _pra + " " +_obj;
+        //return zk;
     }
 }

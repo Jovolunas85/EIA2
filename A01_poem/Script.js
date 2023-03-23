3ab831c55e80bb5120042332cf9b058164967f47
Medin <115719571+Jovolunas85@users.noreply.github.com>
2023-03-23 12:08:42 +0100

    xfgt

--- A01_poem/Script.js
+++ A01_poem/Script.js
@@ -1,23 +1,29 @@
 "use strict";
 var Aufgabe1;
 (function (Aufgabe1) {
-    let subjekte = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-    let praedikate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstroert"];
-    let objekte = ["Zaubertraenke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let verbs = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstroert"];
+    let objects = ["Zaubertraenke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
     //console.log(subjekte, praedikate, objekte);
     let zk;
-    getVerse(subjekte[Math.floor(Math.random() * 7)], praedikate[Math.floor(Math.random() * 7)], objekte[Math.floor(Math.random() * 7)]);
-    for (let i = subjekte.length; i >= 1; i--) {
+    getVerse(subjects[Math.floor(Math.random() * 7)], verbs[Math.floor(Math.random() * 7)], objects[Math.floor(Math.random() * 7)]);
+    for (let i = subjects.length; i >= 1; i--) {
         //console.log(i);
         //console.log(zk);
     }
     function getVerse(_sub, _pra, _obj) {
         let verse = "";
-        let zufall = Math.floor(Math.random() * subjekte.length);
-        console.log(zufall);
-        verse = "" + subjekte[zufall];
-        console.log(verse + " ");
-        subjekte.splice(zufall, 1);
+        let zufall = Math.floor(Math.random() * subjects.length);
+        let zufall2 = Math.floor(Math.random() * verbs.length);
+        let zufall3 = Math.floor(Math.random() * objects.length);
+        //console.log(zufall);
+        verse = subjects[zufall];
+        subjects.splice(zufall, 1);
+        verse = verse + " " + verbs[zufall2];
+        verbs.splice(zufall, 1);
+        verse = verse + " " + objects[zufall3];
+        objects.splice(zufall3, 1);
+        console.log(verse);
         //zk = _sub+ " " + _pra + " " +_obj;
         //return zk;
     }
--- A01_poem/Script.ts
+++ A01_poem/Script.ts
@@ -1,22 +1,29 @@
 namespace Aufgabe1 {
-    let subjekte: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-    let praedikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstroert"];
-    let objekte: string[] = ["Zaubertraenke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    let subjects: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let verbs: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstroert"];
+    let objects: string[] = ["Zaubertraenke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
     //console.log(subjekte, praedikate, objekte);
     let zk: string;
-    getVerse(subjekte[Math.floor(Math.random() * 7)], praedikate[Math.floor(Math.random() * 7)], objekte[Math.floor(Math.random() * 7)]);
-    for (let i: number = subjekte.length; i >= 1; i--) {
+    getVerse(subjects[Math.floor(Math.random() * 7)], verbs[Math.floor(Math.random() * 7)], objects[Math.floor(Math.random() * 7)]);
+    for (let i: number = subjects.length; i >= 1; i--) {
         //console.log(i);
         //console.log(zk);
     }
     function getVerse(_sub: string, _pra: string, _obj: string) {
         let verse: string = "";
-        let zufall: number = Math.floor(Math.random() * subjekte.length);
-        console.log(zufall);
-        verse = "" + subjekte[zufall];
-        console.log(verse + " ");
-        subjekte.splice(zufall, 1);
+        let zufall: number = Math.floor(Math.random() * subjects.length);
+        let zufall2: number = Math.floor(Math.random() * verbs.length);
+        let zufall3: number = Math.floor(Math.random() * objects.length);
+        //console.log(zufall);
+        verse = subjects[zufall];
+        subjects.splice(zufall, 1);
+        verse = verse + " " + verbs[zufall2];
+        verbs.splice(zufall, 1);
+        verse = verse + " " + objects[zufall3];
+        objects.splice(zufall3, 1);
+        console.log(verse);
         //zk = _sub+ " " + _pra + " " +_obj;
         //return zk;
     }
+
 }
